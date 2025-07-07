baa3fe65dfd9d99ad3ebaa18a7de18c10ee926a1
Luis Alfredo Monroy Villegas <lur0y@hotmail.com>
2025-07-06 21:18:38 -0600

    Se prueban rutas con react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export default function App() {
 
 	return (
 		<>
-			<h1>Proyecto vacio!</h1>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/">
+						<Route index element={<></>} />
+						<Route path="login" element={<></>} />
+						<Route path="*" element={<>404 - Not Found</>} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
