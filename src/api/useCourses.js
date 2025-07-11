0aaef6548e603b461c4d6ade10e8334ad6f5882c
Luis Alfredo Monroy Villegas <lur0y@hotmail.com>
2025-07-11 10:11:37 -0600

    Se agregan endpoints de la API relacionados al CRUD de cursos

--- src/api/useCourses.js
+++ src/api/useCourses.js
@@ -23,7 +23,59 @@ export default function useCourses() {
 
     }
 
+    async function createCourse({ name }) {
 
-    return { getCourses };
+        const url = `${BASE_URL}/courses`;
+        const data = {
+            course_name: name
+        };
+        let courseId = 0;
+        apiCallStart();
+        try {
+            const response = await axios.post(url, data, authOptions);
+            courseId = response.data.id;
+        } catch (error) {
+            apiErrorManager(error);
+        } finally {
+            apiCallEnd();
+        }
+
+        return ({ courseId });
+        
+    }
+
+    async function changeCourseName({ courseId, name }) {
+        
+        const url = `${BASE_URL}/courses/${courseId}`;
+        const data = {
+            course_name: name
+        };
+        apiCallStart();
+        try {
+            await axios.put(url, data, authOptions);
+        } catch (error) {
+            apiErrorManager(error);
+        } finally {
+            apiCallEnd();
+        }
+
+    }
+
+    async function deleteCourse({ courseId }) {
+        
+        const url = `${BASE_URL}/courses/${courseId}`;
+        apiCallStart();
+        try {
+            await axios.delete(url, authOptions);
+        } catch (error) {
+            apiErrorManager(error);
+        } finally {
+            apiCallEnd();
+        }
+
+    }
+
+
+    return { getCourses, createCourse, changeCourseName, deleteCourse };
 
 }
